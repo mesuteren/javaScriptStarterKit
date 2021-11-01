e6d17e871fbedb91bb76bb197289bc087c4eebfd
Mesut Eren <mesut.eren@outlook.com>
2021-11-01 22:59:39 +0300

    01.11.2021 2.mola

--- src/functions.js
+++ src/functions.js
@@ -21,3 +21,36 @@ let sayHello2 = function () {
 }
 
 sayHello2()
+
+function addToCart2(productName, quantity, unitPrice) {
+
+}
+
+addToCart2("Elma",5,10)
+
+let product1 = {productName:"Elma", unitPrice:10, quantity:5}
+function addToCart3(product) {
+    console.log("Ürün : "+product.productName)
+    console.log("Adet : "+product.quantity)
+    console.log("Fiyatı : "+product.unitPrice)
+}
+
+
+addToCart3(product1)
+let product2 = {productName:"Elma", unitPrice:10, quantity:5}
+let product3 = {productName:"Elma", unitPrice:10, quantity:5}
+product2=product3
+product2.productName="Karpuz"
+console.log(product3.productName)
+
+function addToCart4(products) {
+    console.log(products)
+}
+
+let products=[
+    {productName:"Elma", unitPrice:10, quantity:5},
+    {productName:"Armut", unitPrice:10, quantity:5},
+    {productName:"Karpuz", unitPrice:10, quantity:5}
+]
+
+addToCart4(products)
